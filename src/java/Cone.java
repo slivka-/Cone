dd7e4e9181339d031d2214c5e859691df2a57b81
slivka- <slivka104@gmail.com>
2017-10-26 19:45:37 +0200

    Cleaned repository, added cone representation

--- src/java/Cone.java
+++ src/java/Cone.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
  * @author Michał Śliwa
  */
 @WebServlet(urlPatterns =
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletResponse;
 })
 public class Cone extends HttpServlet
 {
-
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -26,40 +25,41 @@ public class Cone extends HttpServlet
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         response.setContentType("text/html;charset=UTF-8");
-        if (request.getMethod().equals("GET"))
+        try (PrintWriter out = response.getWriter())
         {
-            try (PrintWriter out = response.getWriter())
+            if (request.getMethod().equals("GET"))
             {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Servlet Cone</title>");            
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>Servlet Cone at " + request.getContextPath() + "</h1>");
-                out.println("<h1>Method: GET</h1>");
-                out.println("</body>");
-                out.println("</html>");
+                try
+                {
+                    int x = Integer.parseInt(request.getParameter("x"));
+                    int y = Integer.parseInt(request.getParameter("y"));
+                    int z = Integer.parseInt(request.getParameter("z"));
+                    double r = Double.parseDouble(request.getParameter("r"));
+                    ConeModel.getInstance().addDefect(x, y, z, r);
+                }
+                catch(NumberFormatException ex)
+                {
+                    out.print(ex);
+                }
             }
-        }
-        else
-        {
-            try (PrintWriter out = response.getWriter())
+            else if (request.getMethod().equals("POST"))
             {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Servlet Cone</title>");            
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>Servlet Cone at " + request.getContextPath() + "</h1>");
-                out.println("<h1>Method: POST</h1>");
-                out.println("</body>");
-                out.println("</html>");
+                try
+                {
+                    double ra = Double.parseDouble(request.getParameter("ra"));
+                    double rb = Double.parseDouble(request.getParameter("rb"));
+                    double h = Double.parseDouble(request.getParameter("h"));
+                    double c = Double.parseDouble(request.getParameter("c"));
+                    double g = Double.parseDouble(request.getParameter("g"));
+                    ConeModel.getInstance().setConeMeas(ra, rb, h, c, g);
+                }
+                catch(NumberFormatException ex)
+                {
+                    out.print(ex);
+                }
             }
         }
     }
@@ -103,7 +103,103 @@ public class Cone extends HttpServlet
     @Override
     public String getServletInfo()
     {
-        return "ZTP - Zadanie 2 - Cone";
+        return "Short description";
     }// </editor-fold>
+}
 
+/**
+ * Represensts a cone.
+ * Point 0,0,0 is located in the middle of the base
+ * 
+ * @author Michał Śliwa
+ */
+class ConeModel
+{
+    // <editor-fold defaultstate="collapsed" desc="Instancing methods">
+    private static ConeModel instance;
+    
+    private ConeModel() {} //private constructor to prevent instantiation
+    
+    public static ConeModel getInstance()
+    {
+        if(instance == null)
+            instance = new ConeModel();
+        return instance;
+    }
+    // </editor-fold>
+    
+    private double RA; //base radius [m]
+    private double RB; //top radius [m]
+    private double H; //height [m]
+    private double C; //cone material density [kg/m^3]
+    private double G; //defects density [kg/m^3]
+    private final ArrayList<Defect> defects = new ArrayList<>();
+    
+    /**
+     * Calculates mass of the cone
+     * 
+     * @return cones mass in kg
+     */
+    public double calculateMass()
+    {
+        return 0.0;
+    }
+    
+    /**
+     * Sets cone measurements, density and defects density
+     * 
+     * @param ra base radius
+     * @param rb top radius
+     * @param h height
+     * @param c cone material density
+     * @param q defects density
+     */
+    public void setConeMeas(double ra, double rb, double h, double c, double g)
+    {
+        this.RA = ra;
+        this.RB = rb;
+        this.H = h;
+        this.C = c;
+        this.G = g;
+    }
+    
+    /**
+     * Adds a defect to cone at given coordinates, with given radius
+     * 
+     * @param x x coord
+     * @param y y coord
+     * @param z z coord
+     * @param r radius
+     */
+    public void addDefect(int x, int y, int z, double r)
+    {
+        defects.add(new Defect(x, y, z, r));
+    }
+    
+    /**
+     * Represents a spherical defect inside of the cone.
+     */
+    class Defect
+    {
+        private final int X;//x coord of defect
+        private final int Y;//y coord of defect
+        private final int Z;//z coord of defect
+        private final double R;//radius of defect [m]
+        
+        public Defect(int x, int y, int z, double r)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.R = r;
+        }
+
+        public int getX() { return X; }
+
+        public int getY() { return Y; }
+
+        public int getZ() { return Z; }
+
+        public double getR() { return R; }
+    }
 }
